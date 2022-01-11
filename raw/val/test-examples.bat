ae07795a7367990ffdfa3f660e1ada79962b3e40
gkholman <gkenholman+github@gmail.com>
2022-01-10 21:02:27 -0500

    Add and update examples

--- raw/val/test-examples.bat
+++ raw/val/test-examples.bat
@@ -1,5 +1,7 @@
 @echo off
-call validate Extended Invoice ../examples/Acme3099123-extended-invoice.xml
+call validate Core Invoice ../examples/Acme3099123-core-invoice.xml
+call validate Core Invoice ../examples/MP-1-core-invoice.xml
+call validate Core Invoice ../examples/MP-2-core-invoice.xml
 
 echo.
 echo.
--- raw/val/test-examples.sh
+++ raw/val/test-examples.sh
@@ -1,1 +1,3 @@
-sh validate.sh Extended Invoice ../examples/Acme3099123-extended-invoice.xml
+sh validate.sh Core Invoice ../examples/Acme3099123-core-invoice.xml
+sh validate.sh Core Invoice ../examples/MP-1-core-invoice.xml
+sh validate.sh Core Invoice ../examples/MP-2-core-invoice.xml

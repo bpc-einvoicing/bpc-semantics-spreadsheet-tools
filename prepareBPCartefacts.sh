51b33c624452061b5f6e66ad8bbc4fe141e9c933
G. Ken Holman <gkenholman+github@gmail.com>
2022-12-12 10:16:17 -0500

    First run of version 0.6
    This takes 0.5 with the first test version of 0.6

--- prepareBPCartefacts.sh
+++ prepareBPCartefacts.sh
@@ -8,12 +8,11 @@ export platform=$2
 export rawdir=raw
 export targetdir="$1"
 export title="BPC Semantics Group"
-export BPCversion=0.5
+export BPCversion=0.6
 export UBLversion=2.3-os
 export dateTime=$3
-export numberOfProcesses=5
 
-export ssGoogle=https://docs.google.com/spreadsheets/d/16Nj_hW5_T_2Z2Hgd_vJHGx47iwvW-RlA
+export ssGoogle=https://docs.google.com/spreadsheets/d/1I3SesJiZYLp-VUk4xHB1VavKqxXc_Fz2Dx85z6gYClg
 
 bash prepareBPCartefacts-common.sh "$1" "$2" "$3" "$4"
 
